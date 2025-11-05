a0140d9d185d0ca6c876e7d608ec757e0bc4e039
IAlcCamDev <alccamdev@gmail.com>
2025-11-05 14:24:54 +0100

    Feat(CI-6): Terminado Test de PistaRepositoryPostgres

--- apps/backend/__tests__/infrastructure/repositories/PistaRepositoryPostgres.test.js
+++ apps/backend/__tests__/infrastructure/repositories/PistaRepositoryPostgres.test.js
@@ -10,6 +10,7 @@ describe('PistaRepositoryPostgres', () => {
     // Mock del modelo de Sequelize
     mockPistaModel = {
       create: jest.fn(),
+      findByPk: jest.fn(),
     };
 
     // Instanciamos el repositorio con el modelo mockeado
@@ -66,6 +67,56 @@ describe('PistaRepositoryPostgres', () => {
       // Act & Assert
       await expect(repository.crear(datosEntrada)).rejects.toThrow(
         'Error al crear en la base de datos'
+      );
+    });
+  });
+
+  describe('obtenerPorId', () => {
+    it('debería obtener una pista por su ID usando Sequelize', async () => {
+      // Arrange
+      const pistaId = 1;
+
+      const modeloRetornado = {
+        id: 1,
+        nombre: 'El Muro',
+        dificultad: '6a',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      mockPistaModel.findByPk = jest.fn().mockResolvedValue(modeloRetornado);
+
+      // Act
+      const resultado = await repository.obtenerPorId(pistaId);
+
+      // Assert
+      expect(mockPistaModel.findByPk).toHaveBeenCalledWith(pistaId);
+
+      expect(resultado).toBeInstanceOf(Pista);
+      expect(resultado.id).toBe(1);
+      expect(resultado.nombre).toBe('El Muro');
+      expect(resultado.dificultad).toBe('6a');
+    });
+
+    it('debería retornar null si no se encuentra la pista', async () => {
+      const pistaId = 999;
+
+      mockPistaModel.findByPk = jest.fn().mockResolvedValue(null);
+
+      const resultado = await repository.obtenerPorId(pistaId);
+
+      expect(mockPistaModel.findByPk).toHaveBeenCalledWith(pistaId);
+      expect(resultado).toBeNull();
+    });
+
+    it('debería manejar errores al obtener una pista por su ID', async () => {
+      const pistaId = 1;
+
+      const errorEsperado = new Error('Error al obtener de la base de datos');
+      mockPistaModel.findByPk = jest.fn().mockRejectedValue(errorEsperado);
+
+      await expect(repository.obtenerPorId(pistaId)).rejects.toThrow(
+        'Error al obtener de la base de datos'
       );
     });
   });
