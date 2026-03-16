abf4887418cd4320eb3141419b482d4865f2bf13
Melendo <nachomelendo@gmail.com>
2026-03-16 17:36:57 +0100

    Configuración de cache agresivo y añadido favicon

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -45,6 +45,8 @@ export default defineConfig({
         ]
       },
       workbox: {
+        skipWaiting: true,
+        clientsClaim: true,
         navigateFallback: '/index.html',
         navigateFallbackDenylist: [/^\/(escaladores|pistas|rocodromos|zonas)\b/],
         runtimeCaching: [
